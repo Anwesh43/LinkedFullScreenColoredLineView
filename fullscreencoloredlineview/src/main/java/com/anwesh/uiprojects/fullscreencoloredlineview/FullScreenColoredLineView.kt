7e0cca0ce6ff333a12eb9f458e0ce76c1a763265
Anwesh43 <anweshthecool0@gmail.com>
2020-06-26 23:52:44 +0530

    adding FSCLNode class

--- fullscreencoloredlineview/src/main/java/com/anwesh/uiprojects/fullscreencoloredlineview/FullScreenColoredLineView.kt
+++ fullscreencoloredlineview/src/main/java/com/anwesh/uiprojects/fullscreencoloredlineview/FullScreenColoredLineView.kt
@@ -119,4 +119,45 @@ class FullScreenColoredLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FSCLNode(var i : Int, val state : State = State()) {
+
+        private var next : FSCLNode? = null
+        private var prev : FSCLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FSCLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFSCLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FSCLNode {
+            var curr : FSCLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
