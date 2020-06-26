725478ff591f62c5a696e41b474b32474c90ae2f
Anwesh43 <anweshthecool0@gmail.com>
2020-06-26 23:42:40 +0530

    adding FullScreneColoredLineView class

--- fullscreencoloredlineview/src/main/java/com/anwesh/uiprojects/fullscreencoloredlineview/FullScreenColoredLineView.kt
+++ fullscreencoloredlineview/src/main/java/com/anwesh/uiprojects/fullscreencoloredlineview/FullScreenColoredLineView.kt
@@ -55,3 +55,20 @@ fun Canvas.drawFSCLNode(i : Int, scale : Float, paint : Paint) {
     drawFullScreenColoredLine(scale, w, h, paint)
     restore()
 }
+
+
+class FullScreenColoredLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
