56ed107fa8f92ef26e54a3d12390b69d4b53b7a1
Anwesh43 <anweshthecool0@gmail.com>
2020-06-26 23:58:31 +0530

    adding static create method

--- fullscreencoloredlineview/src/main/java/com/anwesh/uiprojects/fullscreencoloredlineview/FullScreenColoredLineView.kt
+++ fullscreencoloredlineview/src/main/java/com/anwesh/uiprojects/fullscreencoloredlineview/FullScreenColoredLineView.kt
@@ -208,4 +208,12 @@ class FullScreenColoredLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : FullScreenColoredLineView {
+            val view : FullScreenColoredLineView = FullScreenColoredLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
