e7425de9528897ad81c72635b9513d9829348614
Anwesh43 <anweshthecool0@gmail.com>
2020-06-26 23:59:54 +0530

    adding fullscrene support

--- app/src/main/java/com/anwesh/uiprojects/linkedfullscreencoloredlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedfullscreencoloredlineview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedfullscreencoloredlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.fullscreencoloredlineview.FullScreenColoredLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FullScreenColoredLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
